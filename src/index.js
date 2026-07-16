7b5c9a8ba8fb371bbad2075be919de9b3ab8f5ce
cha0s <git@cha0s.io>
2026-07-16 18:11:20 -0500

    preparation for 2.0.0

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './primitives.js';
-
-import './array.js';
-import './map.js';
-import './object.js';
-export { Index, Pool } from './pool.js';
-export { Property } from './property.js';
-export { Diff, Instance, MarkClean, Set, SetWithDefaults, ToJSON, ToJSONWithoutDefaults } from './proxy.js';
-export { registry } from './register.js';
